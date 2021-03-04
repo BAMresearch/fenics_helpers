954c5f6267e1bb2c05e41a7307bc13b9c706f00d
Thomas Titscher <thomas.titscher@bam.de>
2021-03-04 11:36:53 +0100

    Ignore import error of .plotting
    In CI setups, matplotlib is often not present, but required by
    .plotting. So importing it by default should not cause errors.

--- fenics_helpers/__init__.py
+++ fenics_helpers/__init__.py
@@ -1,4 +1,7 @@
 import fenics_helpers.boundary
 import fenics_helpers.timestepping
-import fenics_helpers.plotting
 import fenics_helpers.rk
+try:
+    import fenics_helpers.plotting
+except ImportError as e:
+    print("Skip import of plotting module." + e)
